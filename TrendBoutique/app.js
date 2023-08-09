7f6307044fd48b81361de22cf51c215afd9c1aa6
DavidAlcalde1 <david20egt@gmail.com>
2023-08-08 19:10:09 -0500

    update Sprint3

--- TrendBoutique/app.js
+++ TrendBoutique/app.js
@@ -7,9 +7,17 @@ const router = express.Router();
 
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
+
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
+    res.sendFile(path.join(__dirname, './views/home.ejs'));
+});
+
+app.get('/', (req, res)=> {
+    res.render('home')
 });
 
 app.use(router);
+app.use(express.static('public'));
 app.listen(3000, ()=> console.log('Servidor Corriendo'));
+
