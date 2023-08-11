6b06a3422c0b4313fb9045d1a8bc4e141226a7e0
DavidAlcalde1 <david20egt@gmail.com>
2023-08-11 18:52:01 -0500

    Update ruteo terminado

--- TrendBoutique/app.js
+++ TrendBoutique/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+const rutaHome = require('./routes/home');
+const rutaCarrito = require('./routes/carrito');
+const rutaDetalle = require('./routes/detalle');
+const rutaLogin = require('./routes/login');
+const rutaRegistro = require('./routes/registro');
+
+    
 const app = express();
 
 const router = express.Router();
@@ -9,49 +16,16 @@ app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.ejs'));
-});
-
-app.get('/', (req, res)=> {
-    res.render('home')
-});
-
-router.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/carrito.ejs'));
-});
-
-app.get('/carrito', (req, res)=> {
-    res.render('carrito')
-});
-
-router.get('/detalle', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/detalle.ejs'));
-});
-
-app.get('/detalle', (req, res)=> {
-    res.render('detalle')
-});
-
-router.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.ejs'));
-});
-
-app.get('/login', (req, res)=> {
-    res.render('login')
-});
-
-router.get('/registro', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/registro.ejs'));
-});
-
-app.get('/registro', (req, res)=> {
-    res.render('registro')
-});
+// Montar el enrutador de home
+app.use('/', rutaHome);
+app.use('/', rutaCarrito);
+app.use('/', rutaDetalle);
+app.use('/', rutaLogin);
+app.use('/', rutaRegistro);
 
 
 
 app.use(router);
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.listen(3000, ()=> console.log('Servidor Corriendo'));
 
