8da52a6c6805a6eaabcd5448eb718056cb40f4e5
unknown <david182_segura@hotmail.com>
2023-08-11 19:02:52 -0500

    subida

--- TrendBoutique/app.js
+++ TrendBoutique/app.js
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.ejs'));
 });
 
-app.get('/', (req, res)=> {
+router.get('/', (req, res)=> {
     res.render('home')
 });
 
--- TrendBoutique/routes/home.js
+++ TrendBoutique/routes/home.js
@@ -1,8 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.ejs'));
 });
 
-app.get('/', (req, res)=> {
+router.get('/', (req, res)=> {
     res.render('home')
 });
 
+module.exports = router
