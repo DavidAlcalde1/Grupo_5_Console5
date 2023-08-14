3479a2a46f9c470341feec90b346fc82842761c6
DavidAlcalde1 <david20egt@gmail.com>
2023-08-13 19:49:19 -0500

    Update avance Sprint4

--- TrendBoutique/app.js
+++ TrendBoutique/app.js
@@ -6,11 +6,12 @@ const rutaCarrito = require('./routes/carrito');
 const rutaDetalle = require('./routes/detalle');
 const rutaLogin = require('./routes/login');
 const rutaRegistro = require('./routes/registro');
+const rutaProducts = require('./routes/products');
 
     
 const app = express();
 
-const router = express.Router();
+// const router = express.Router();
 
 app.use(express.static('public'));
 
@@ -20,8 +21,9 @@ app.use('/', rutaCarrito);
 app.use('/', rutaDetalle);
 app.use('/', rutaLogin);
 app.use('/', rutaRegistro);
+app.use('/', rutaProducts);
 
-app.use(router);
+// app.use(router);
 // app.use(express.static('public'));
 app.listen(3000, ()=> console.log('Servidor Corriendo'));
 
---
+++ TrendBoutique/routes/products.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const productsData = require('../public/data/products.json'); // Asegúrate de tener acceso a los datos de productos
+
+router.get('/list', (req, res) => {
+    res.render('products/list', { products: productsData });
+});
+
+router.get('/products/create', (req, res) => {
+    res.render('products/create');
+});
+
+router.get('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    // Encuentra el producto correspondiente en productsData usando productId
+    const product = productsData.find(product => product.id === productId);
+    res.render('products/detail', { product });
+});
+
+router.post('/products', (req, res) => {
+    const newProduct = req.body; // Los datos del nuevo producto enviados desde el formulario
+    // Agregar lógica para agregar el nuevo producto a productsData
+    // Guardar productsData en tu fuente de datos JSON
+    res.redirect('/products'); // Redirige de vuelta a la lista de productos
+});
+
+router.get('/products/:id/edit', (req, res) => {
+    const productId = req.params.id;
+    // Encuentra el producto correspondiente en productsData usando productId
+    const product = productsData.find(product => product.id === productId);
+    res.render('products/edit', { product });
+});
+
+router.put('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const updatedProductData = req.body; // Los datos editados del producto
+    // Encuentra y actualiza el producto correspondiente en productsData usando productId
+    // Guardar productsData en tu fuente de datos JSON
+    res.redirect(`/products/${productId}`); // Redirige de vuelta al detalle del producto
+});
+
+router.delete('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    // Encuentra y elimina el producto correspondiente en productsData usando productId
+    // Guardar productsData en tu fuente de datos JSON
+    res.redirect('/products'); // Redirige de vuelta a la lista de productos
+});
+
+
+module.exports = router;
