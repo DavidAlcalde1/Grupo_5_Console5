074c2670923709dab445017344a5dc4adcb71c4b
DavidAlcalde1 <david20egt@gmail.com>
2023-08-17 22:02:41 -0500

    cambios en ControllerProducts.js

--- TrendBoutique/controllers/controllerProduct.js
+++ TrendBoutique/controllers/controllerProduct.js
@@ -1,6 +1,11 @@
+const productsData = require('../public/data/products.json');
+
+
+
+
 const controllerProducts = {
     listar : (req, res) => {
-        res.send('Lista de productos Dvaico')
+        res.send(res.render('products/list', { products: productsData }));
     },
     detalle: (req, res) => {
         res.send('Detalle del producto')
