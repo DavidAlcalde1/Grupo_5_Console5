01551e4bd5bb3bd37833c40e8d27e92438bbe6ed
unknown <david182_segura@hotmail.com>
2023-08-22 19:47:39 -0500

    finalizado list e inicio de detail

--- TrendBoutique/controllers/controllerProduct.js
+++ TrendBoutique/controllers/controllerProduct.js
@@ -2,16 +2,22 @@ const products = require('../public/data/products.json');
 
 
 const controllerProducts = {
-    listar : (req, res) => {
-        console.log(req.query.category);
-        const productsByCategory = products.filter( (product) => {
-            return product.category === req.query.category;
-        });
-        res.render('products/list', { products: productsByCategory })
-    },
+    listar: (req, res) => {
+        if (typeof req.query.category === 'string') {
+          let filtro = products.filter((item) => {
+            return item.category === req.query.category
+          })
+          res.render('products/list', {products: filtro})
+        } else res.render('products/list', {products})    
+      },
     detalle: (req, res) => {
-        res.render('products/detail')
-    },
+        if (typeof req.query.category === 'string') {
+          let filtro = products.filter((item) => {
+            return item.category === req.query.category
+          })
+          res.render('products/detail', {products: filtro})
+        } else res.render('products/detail', {products})    
+      },
     crear: (req, res) => {
         res.render('products/create')
     },
