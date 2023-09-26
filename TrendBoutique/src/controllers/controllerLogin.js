539cad16870c6f7d5d76e8792d275e1abee61000
unknown <david182_segura@hotmail.com>
2023-09-25 21:29:53 -0500

    creación de Login, controllers, router, views

--- TrendBoutique/src/controllers/controllerLogin.js
+++ TrendBoutique/src/controllers/controllerLogin.js
@@ -1,9 +1,11 @@
 const path = require ('path');
 const fs = require ('fs');
 
+let login = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'data', 'users.json')));
+
 const controllerLogin = {
     show: (req, res) => {
-        let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'data', 'products.json')))
+        let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'data', 'users.json')))
         res.render (path.resolve(__dirname, '..', 'views', 'users', 'login'), {products})
     }
 } 
--- TrendBoutique/src/routes/login.js
+++ TrendBoutique/src/routes/login.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, path.resolve(__dirname, '../../public/images'));
+    },
+    filename: function(req,file,cb) {
+        cb(null, 'user-' + Date.now() + path.extname(file.originalname)); 
+    }
+})
+
+const upload = multer({storage : storage});
 
 const controllerLogin = require(path.resolve(__dirname, '..', 'controllers', 'controllerLogin'))
 
 router.get('/login', controllerLogin.show)
+router.get('/login/edit/:id', controllerLogin.edit);
 
 module.exports = router
