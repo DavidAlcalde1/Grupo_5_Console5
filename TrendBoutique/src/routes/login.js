d1eea9dea1cce95856acc13190ce62cc27e2b9ac
unknown <david182_segura@hotmail.com>
2023-09-28 20:41:33 -0500

    validaciones login

--- TrendBoutique/src/routes/login.js
+++ TrendBoutique/src/routes/login.js
@@ -24,14 +24,9 @@ let validation = [
     body('password').custom((value, { req }) => {
         for (let i = 0; i < users.length; i++) {
             if (users[i].email == req.body.email) {
-                console.log(users[i].email);
                 if (bcrypt.compareSync(value, users[i].password)) {
-                    console.log(bcrypt.hashSync(value, 10));
-                    console.log(users[i].password);
                     return true
                 } else {
-                    console.log(bcrypt.hashSync(value, 10));
-                    console.log(users[i].password);
                     return false
                 }
             }
@@ -39,8 +34,12 @@ let validation = [
     }).withMessage('clave invalida')
 ]
 
+let validationRegister = [
+
+]
+
 router.get('/login', controllerLogin.login);
-router.post('/login', validation, controllerLogin.getIn);
+router.post('/login',validation, controllerLogin.getIn);
 router.get('/register', controllerLogin.register);
 router.post('/register', upload.single('image'), controllerLogin.create);
 
