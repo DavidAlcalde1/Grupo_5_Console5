175f5541fac2daade4659bf7bb82138c43194ac1
DavidAlcalde1 <david20egt@gmail.com>
2023-10-23 20:38:09 -0500

    Validadores front terminados

--- TrendBoutique/public/js/editValidators.js
+++ TrendBoutique/public/js/editValidators.js
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 window.addEventListener('load', function () {
     console.log('Evento load se ha disparado correctamente.');
     let formEdit = document.querySelector('form.edit')
+    console.log('Este es FORM EDIT',formEdit)
     formEdit.addEventListener('submit', function (e) {
         let errores = [];
         
@@ -23,7 +23,7 @@ window.addEventListener('load', function () {
         }   
         // Validaciones de la Imagen
         let campoImagen = document.querySelector('#image');
-        if (campoImagen.files.length === 0) {
+        if (!campoImagen.files) {
             errores.push("¡Debes seleccionar una imagen!");
         } else {
             // Verificar la extensión del archivo
@@ -37,7 +37,9 @@ window.addEventListener('load', function () {
         }
                 
 
+        console.log(errores)
         if (errores.length > 0) {
+            console.log("prevent default")
             e.preventDefault(); // Evita que el formulario se envíe si hay errores.
         
             let ulErrores = document.querySelector("div.errores ul");
@@ -53,60 +55,5 @@ window.addEventListener('load', function () {
         }
 
     })
-=======
-window.addEventListener('load', function () {
-    console.log('Evento load se ha disparado correctamente.');
-    let formEdit = document.querySelector('form.edit')
-    formEdit.addEventListener('submit', function (e) {
-        let errores = [];
-        
-        let campoNombre = document.querySelector('#name');
-        
-        if (campoNombre.value.trim() === "") {
-            errores.push("¡El campo de Nombre debe estar llenado!!!");
-        
-        } else if (campoNombre.value.length < 5 ) {
-            errores.push("¡Solo se permiten mínimo 05 caracteres para el campo Nombre!!!");
-        }
-        // Validaciones de Descripción
-        let campoDescripcion = document.querySelector('#description');
-        
-        if (campoDescripcion.value.trim() === "") {
-            errores.push( "¡El campo de Apellido debe estar llenado¡¡¡")
-        } else if (campoDescripcion.value.length < 20 ) {
-            errores.push( "¡El campo Descripción deberá tener al menos 20 caracteres!!!");
-        }   
-        // Validaciones de la Imagen
-        let campoImagen = document.querySelector('#image');
-        if (campoImagen.files.length === 0) {
-            errores.push("¡Debes seleccionar una imagen!");
-        } else {
-            // Verificar la extensión del archivo
-            const allowedExtensions = ["jpg", "jpeg", "png", "gif"];
-            const fileName = campoImagen.files[0].name;
-            const fileExtension = fileName.split(".").pop().toLowerCase();
 
-            if (!allowedExtensions.includes(fileExtension)) {
-                errores.push("¡La imagen debe ser de formato JPG, JPEG, PNG o GIF!");
-            }
-        }
-                
-
-        if (errores.length > 0) {
-            e.preventDefault(); // Evita que el formulario se envíe si hay errores.
-        
-            let ulErrores = document.querySelector("div.errores ul");
-            ulErrores.innerHTML = ""; // Limpia cualquier contenido previo.
-        
-            for (let i = 0; i < errores.length; i++) {
-                let li = document.createElement("li");
-                let br = document.createElement("br");
-                li.textContent = errores[i];
-                ulErrores.appendChild(li);
-                ulErrores.appendChild(br);
-            }
-        }
-
-    })
->>>>>>> 868be0ba27c78907b4ac009e3125d17ce1a60526
 })
